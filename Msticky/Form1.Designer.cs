ea9fff32947f9323015544a2f5b6f5bf298e6583
matsuko <mononoco@hotmail.co.jp>
2010-01-18 09:51:14 +0900

    add basic functions.
    -Select File and Open.
    -Change Opacity. Right Key, Left Key.
    -Change TopMost.

--- Msticky/Form1.Designer.cs
+++ Msticky/Form1.Designer.cs
@@ -28,8 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -40,19 +45,48 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.topToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(131, 48);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.openToolStripMenuItem.Text = "Open File";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // topToolStripMenuItem
+            // 
+            this.topToolStripMenuItem.Checked = true;
+            this.topToolStripMenuItem.CheckOnClick = true;
+            this.topToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.topToolStripMenuItem.Name = "topToolStripMenuItem";
+            this.topToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.topToolStripMenuItem.Text = "Top";
+            this.topToolStripMenuItem.Click += new System.EventHandler(this.topToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 271);
+            this.ContextMenuStrip = this.contextMenuStrip1;
             this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Opacity = 0.5;
-            this.Text = "Form1";
+            this.Text = "Msticky";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -60,6 +94,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem topToolStripMenuItem;
     }
 }
 
--- Msticky/Form1.cs
+++ Msticky/Form1.cs
@@ -11,6 +11,8 @@ namespace Msticky
 {
     public partial class Form1 : Form
     {
+        Bitmap bitmap;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,13 +21,49 @@ namespace Msticky
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Bitmap bitmap = new Bitmap(@"C:\tmp\a.bmp");
+            bitmap = null;
+            this.TopMost = true;
+        }
+
+        private void SetImage(String file)
+        {
+            if (bitmap != null)
+                bitmap.Dispose();
+            bitmap = new Bitmap(file);
 
             pictureBox1.Image = bitmap;
             pictureBox1.Location = new Point(0, 0);
             pictureBox1.Size = bitmap.Size;
 
             this.ClientSize = bitmap.Size;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image(*.bmp;*.png;*.gif;*.jpg;*.jpeg)|*.bmp;*.png;*.gif;*.jpg;*.jpeg";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                SetImage(ofd.FileName);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    this.Opacity += 0.1;
+                    break;
+                case Keys.Left:
+                    this.Opacity -= 0.1;
+                    break;
+            }
+        }
+
+        private void topToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.TopMost = !this.TopMost;
         }
     }
 }
